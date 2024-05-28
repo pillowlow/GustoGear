d11615fd05bf5d9dca4aba69cee9b88fa35fac33
RyoyakS <74259698+RyoyakS@users.noreply.github.com>
2024-05-28 20:04:49 +0800

    Timer UI added

---
+++ Assets/6YA/scripts/Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Image UIFill;
+    [SerializeField] private TMP_Text UIText;
+    public int duration;
+    private int remainingTime;
+    private bool Pause;
+    private void Start()
+    {
+        Being(duration);
+    }
+    private void Being(int second)
+    {
+        remainingTime = second;
+        StartCoroutine(UpdateTimer());
+    }
+    private IEnumerator UpdateTimer()
+    {
+        while (remainingTime >= 0)
+        {
+            if (!Pause)
+            {
+                UIText.text = $"{remainingTime / 60:00}:{remainingTime % 60:00}";
+                UIFill.fillAmount = Mathf.InverseLerp(0, duration, remainingTime);
+                remainingTime--;
+                yield return new WaitForSeconds(1);
+            }
+            yield return null;
+        }
+        OnEnd();
+    }
+    private void OnEnd()
+    {
+        print("Time's up!");
+    }
+}
