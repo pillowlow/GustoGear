98658ccf98c130b254d7df276776376dce443736
RyoyakS <74259698+RyoyakS@users.noreply.github.com>
2024-06-01 19:06:41 +0800

    UI with flavor interact

--- Assets/6YA/scripts/CustomSocket.cs
+++ Assets/6YA/scripts/CustomSocket.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using UnityEngine;
 using Oculus.Interaction;
+using TMPro;
 
 public class CustomSocket : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject attachPointOnSocket;
-    [SerializeField]
-    private GameObject UnitSet;
+    [SerializeField] private GameObject attachPointOnSocket;
+    [SerializeField] private GameObject UnitSet;
+    [SerializeField] private TMP_Text FlavorText;
+    [SerializeField] private TMP_Text TextureText;
+
     private bool isOccupied = false;
+
+    private float coolDownTime = 0.5f;
+
     private void OnTriggerEnter(Collider other)
     {
         if (isOccupied) return;  // 如果已经被占用，直接返回
         if (other.CompareTag("ColorUnit"))
         {
+            ColorUnit colorUnit = other.GetComponent<ColorUnit>();
+            TasteType CurrentTaste = colorUnit.Taste;
+            FlavorText.text = CurrentTaste.ToString();
 
+            TextureText.text = colorUnit.currentState.ToString();
+            Debug.Log("CurrentTaste: " + CurrentTaste);
             if (attachPointOnSocket != null)
             {
                 other.transform.position = attachPointOnSocket.transform.position;
@@ -26,7 +36,7 @@ public class CustomSocket : MonoBehaviour
                     rb.useGravity = false;
                 }
                 isOccupied = true;
-                Debug.Log("Object attached: isKinematic=" + rb.isKinematic + ", useGravity=" + rb.useGravity);
+                // Debug.Log("Object attached: isKinematic=" + rb.isKinematic + ", useGravity=" + rb.useGravity);
             }
             else
             {
@@ -37,8 +47,9 @@ public class CustomSocket : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("ColorUnit"))
+        if (isOccupied && other.CompareTag("ColorUnit"))
         {
+
             other.transform.parent = UnitSet.transform;
             Rigidbody rb = other.GetComponent<Rigidbody>();
             if (rb != null)
@@ -46,8 +57,15 @@ public class CustomSocket : MonoBehaviour
                 rb.isKinematic = false;
                 rb.useGravity = true;
             }
+            //cool down
+            FlavorText.text = "null";
+            StartCoroutine(CoolDown());
             isOccupied = false;
-            Debug.Log("Object detached: isKinematic=" + rb.isKinematic + ", useGravity=" + rb.useGravity);
+            // Debug.Log("Object detached: isKinematic=" + rb.isKinematic + ", useGravity=" + rb.useGravity);
         }
+    }
+    private IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(coolDownTime);
     }
 }
